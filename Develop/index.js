55e6ea322295bbe146dc0846add4020fe55300c3
jayasreeyuvi@ <114691852+jayasreeyuvi@users.noreply.github.com>
2023-03-04 23:56:11 -0400

    Add files via upload

---
+++ Develop/index.js
@@ -0,0 +1,13 @@
+// TODO: Include packages needed for this application
+//file system//
+const fs = require('fs');
+//inquirer file//
+const inquirer = require('inquirer');
+// generateMarkdown.js// 
+const generateMarkdown = require('./utils/generateMarkdown.js')
+// console Log path//
+
+console.log("Welcome to My README PROFILE")
+
+
+// TODO: Create an array of questions for user input
